9f8e1fb8c4e28ee837a364997480462a90327229
RicardoMicrosoft365 <josericardo1623@gmail.com>
2025-04-07 14:27:48 -0300

    Implementação da funcionalidade de Consulta de Análises Salvas. Checkpoint importante no projeto.

--- src/types/custom.d.ts
+++
@@ -1,4 +0,0 @@
-// Declaração para módulos do Material UI
-declare module '@mui/material';
-declare module '@mui/icons-material/CloudUpload';
-declare module '@mui/material/CircularProgress'; 
--- src/types/global.d.ts
+++
@@ -1,5 +0,0 @@
-// Declaração de tipos para o leaflet.heat
-declare module 'leaflet.heat' {
-  const heat: any;
-  export = heat;
-} 
